3f7e09ef0c82368d6a31ad41448f3db21aac1032
Emerson <Emerson@nilton>
2021-06-04 14:36:16 -0300

    Alterações

--- Teste/src/main/java/Java8Streams.java
+++ Teste/src/main/java/Java8Streams.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Java8Streams {
 	
@@ -7,13 +8,13 @@ public class Java8Streams {
 		
 		List<Integer> lista = Arrays.asList(1,5,8,9,1,4,7,6,6,9,9);
 		
-//		for(Integer x: lista) {
-//			System.out.println(x);
-//		}
 		
-		lista.stream()
-			.filter(e -> e/2 == 3 && e%2 == 0)
-			.map(e -> e*2)
-			.forEach(e -> System.out.println(e));
+		String collect = lista.stream()
+			.filter(e -> e%2 == 0)
+			.distinct()
+			.map(e -> String.valueOf(e))
+			.collect(Collectors.joining("|"));
+		
+		System.out.println(collect);
 	}
 }
